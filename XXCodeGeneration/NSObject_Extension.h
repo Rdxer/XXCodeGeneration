d470f1361c40a460bd514f2a1d51672b8aa06a37
李晓枫 <rdxer@foxmail.com>
2016-01-19 16:49:56 +0800

    Initial Commit

---
+++ XXCodeGeneration/NSObject_Extension.h
@@ -0,0 +1,16 @@
+//
+//  NSObject_Extension.h
+//  XXCodeGeneration
+//
+//  Created by LXF on 16/1/19.
+//  Copyright © 2016年 LXF. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (Xcode_Plugin_Template_Extension)
+
++ (void)pluginDidLoad:(NSBundle *)plugin;
+
+
+@end
---
+++ XXCodeGeneration/XXCodeGeneration.h
@@ -0,0 +1,21 @@
+//
+//  XXCodeGeneration.h
+//  XXCodeGeneration
+//
+//  Created by LXF on 16/1/19.
+//  Copyright © 2016年 LXF. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@class XXCodeGeneration;
+
+static XXCodeGeneration *sharedPlugin;
+
+@interface XXCodeGeneration : NSObject
+
++ (instancetype)sharedPlugin;
+- (id)initWithBundle:(NSBundle *)plugin;
+
+@property (nonatomic, strong, readonly) NSBundle* bundle;
+@end
